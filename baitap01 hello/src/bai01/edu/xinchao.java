2a58de942285f9a1971e20d73c26409a94ee4c56
Cuong0399904438 <cuongtuyen18052014@gmail.com>
2024-09-24 08:59:52 +0700

    baitap1
    hello

---
+++ baitap01 hello/src/bai01/edu/xinchao.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package bai01.edu;
+
+/**
+ * 
+ */
+public class xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("HELLO");
+		
+
+	}
+
+}
