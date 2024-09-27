25d5ad2394d2abeb1d6c91e16db280aeceb0782c
Cuong0399904438 <cuongtuyen18052014@gmail.com>
2024-09-27 08:49:57 +0700

    bytesangbit

---
+++ bytesangbit/src/bytesangbit/doibytesangbit.java
@@ -0,0 +1,15 @@
+package bytesangbit;
+
+public class doibytesangbit {
+
+		public class ByteToBitConverter {
+		    public static void main(String[] args) {
+		        int bytes = 5; // Ví dụ: 5 bytes
+		        int bits = bytes * 8; // 1 byte = 8 bits
+		        System.out.println(bytes + " bytes = " + bits + " bits");
+		    }
+		}
+
+	}
+
+
