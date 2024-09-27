616e72cc4a9679fec51a4c7bb647be3617867bce
Cuong0399904438 <cuongtuyen18052014@gmail.com>
2024-09-27 08:37:17 +0700

    hinhtron

---
+++ chuvihinhtron/src/chuvihinhtron/hinhtron.java
@@ -0,0 +1,22 @@
+package chuvihinhtron;
+
+public class hinhtron {
+
+	public static void main(String[] args) {
+		import java.util.Scanner;
+
+		public class CircleCircumference {
+		    public static void main(String[] args) {
+		        Scanner scanner = new Scanner(System.in);
+		        
+		        System.out.print("Nhập bán kính hình tròn: ");
+		        double radius = scanner.nextDouble();
+		        
+		        double circumference = 2 * Math.PI * radius;
+		        
+		        System.out.println("Chu vi hình tròn là: " + circumference);
+		    }
+
+
+	}
+
